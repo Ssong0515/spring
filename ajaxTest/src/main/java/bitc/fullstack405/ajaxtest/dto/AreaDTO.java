ea56a1f85f38ed96fd012e8de20e149359cbdf8b
Ssong0515 <406605@naver.com>
2024-07-15 18:32:42 +0900

    게시판 로그인2
    스프링을 이용하여 게시판
    1. 원래 방식대로 service(controller)에서 로그인 처리
    2. javascript로 브라우저에서 직접 처리
    3. interceptor를 이용하여 중간에 로그인 가로채서 처리

--- ajaxTest/src/main/java/bitc/fullstack405/ajaxtest/dto/AreaDTO.java
+++ ajaxTest/src/main/java/bitc/fullstack405/ajaxtest/dto/AreaDTO.java
@@ -8,13 +8,12 @@ import java.util.List;
 @Data
 public class AreaDTO {
     private String areaName;
-    private ArrayList<String> subAreas;
 
     public AreaDTO(String areaName) {
         this.areaName = areaName;
     }
 
-    private List<String> subAreaNames() {
+    public List<String> subAreaNames() {
         List<String> subAreas = new ArrayList<>();
 
         subAreas.add(this.areaName + "동구");
@@ -22,12 +21,10 @@ public class AreaDTO {
         subAreas.add(this.areaName + "남구");
         subAreas.add(this.areaName + "북구");
 
-        subAreas.add(this.areaName);
-
         return subAreas;
     }
 
-    private List<String> subSubAreaNames(String subAreaName){
+    public List<String> subSubAreaNames(String subAreaName){
         List<String> subSubAreas = new ArrayList<>();
 
         for (int i = 1; i < 5; i++) {
